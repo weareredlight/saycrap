70d52f31383d69cb4288ae9ee65014724d47ff6d
Tony Goncalves <tony@redlightsoft.com>
2017-12-07 17:31:10 +0000

    wip

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { createStore, applyMiddleware } from 'redux';
-import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import mainReducer from './reducers';
-import Crapper from './Crapper';
 
-const store = createStore(
-  mainReducer,
-  applyMiddleware(thunk)
-);
+const Item = ({ id, text }) => {
+  return <li key={id}>{text}</li>;
+};
 
-export default function App() {
-  return <Provider store={store}><Crapper/></Provider>;
+
+class App extends Component {
+
+  state = {
+    messages: [
+      { id: 1, text: 'cenas' },
+      { id: 2, text: 'fixes' }
+    ]
+  }
+
+  componentWillMount() {
+    fetch('http://localhost:3000/craps.json')
+      .then((r) => r.json())
+      .then((json) => this.setState({ messages: json }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <ul>
+        {this.state.messages.map(Item)}
+        </ul>
+      </div>
+    );
+  }
 }
+
+export default App;
